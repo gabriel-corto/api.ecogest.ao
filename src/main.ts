b201d93dea30fb895a86287cb5874188290c2f1a
gabriel-corto <digital.gabriel2006@gmail.com>
2025-10-20 09:46:03 +0100

    feat: refactor and remove swagger mapping

--- src/main.ts
+++ src/main.ts
@@ -3,31 +3,18 @@ import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
 
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  //Swagger
-  const config = new DocumentBuilder()
-    .setTitle('api.ecogest.ao')
-    .setDescription('Welcome to ecogest api documentation')
-    .setVersion('1.0')
-    .build();
-  const documentFactory = () => SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, documentFactory);
-
-  //Nestjs Validation
   app.useGlobalPipes(new ValidationPipe());
 
-  //Nestjs Cors
   app.enableCors({
     origin: process.env.CLIENT_URL,
     credentials: true,
   });
 
-  //Nestjs CookieParser
   app.use(cookieParser());
 
   await app.listen(process.env.PORT ?? 3000);
